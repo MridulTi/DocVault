c549ee1a1abeceb82eed02d265b876117cc5a006
Ankiiisharma <myselfankit.2003@gmail.com>
2024-03-28 13:45:20 +0530

    test

--- contracts/Upload.sol
+++ contracts/Upload.sol
@@ -7,13 +7,7 @@ contract Upload {
      address user; 
      bool access; //true or false
   }
-  struct Person{
-    address user;
-    string[] Access;
-  }
   mapping(address=>string[]) value;
-  mapping(address=>string[])friends;
-  mapping(address=>Person)person;
   mapping(address=>mapping(address=>bool)) ownership;
   mapping(address=>Access[]) accessList;
   mapping(address=>mapping(address=>bool)) previousData;
@@ -44,24 +38,10 @@ contract Upload {
       }
   }
 
-      function display(address _user) external view returns(string[] memory){
+  function display(address _user) external view returns(string[] memory){
       require(_user==msg.sender || ownership[_user][msg.sender],"You don't have access");
       return value[_user];
   }
-//   function display(address user) external view returns(string memory){
-//     if(user==msg.sender || ownership[user][msg.sender]){
-// 
-//     }else{
-        
-//     }
-    //   require(user==msg.sender || ownership[user][msg.sender],"You don't have access");
-      
-    function AccessFriends() public view returns(string[] memory){
-        return person[msg.sender].Access;
-    }
-    function MyFriends() public view returns(string[] memory){
-        return friends[msg.sender];
-    }
 
   function shareAccess() public view returns(Access[] memory){
       return accessList[msg.sender];
